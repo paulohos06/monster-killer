fa28fa832517409fc6829b87459f96e22ab5973d
Paulo Henrique <paulo.henrique06@hotmail.com>
2020-04-07 20:59:30 -0300

    add attack methods

--- app.js
+++ app.js
@@ -19,9 +19,24 @@ new Vue({
     giveUpGame() {
       this.running = !this.running
       this.playerLife = 0
+    },
+    attack(special) {
+      this.hurt('playerLife', 7, 12, false)
+      this.hurt('monsterLife', 5, 10, special)
+    },
+    hurt(fighter, min, max, flag) {
+      const plus = flag ? 7 : 0
+      const hurt = this.getRandom((min + plus), (max + plus))
+      this[fighter] = Math.max(this[fighter] - hurt, 0)
+    },
+    getRandom(min, max) {
+      const value = Math.random() * (max - min) + min
+      return Math.round(value)
     }
   },
   watch: {
-    
+    hasResult(life) {
+      if (life) this.running = false
+    }
   }
 })
